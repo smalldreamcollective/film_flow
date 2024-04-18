bc5ede760a0494e305c23b51d72cf1f0d14ddc3e
Michael Wheelwright <michael.wheelwright@artescolectivos.attlocal.net>
2024-04-17 18:47:36 -0700

    add initial url validation

--- lib/film_flow/settings/manufacturer.ex
+++ lib/film_flow/settings/manufacturer.ex
@@ -2,6 +2,7 @@ defmodule FilmFlow.Settings.Manufacturer do
   use Ecto.Schema
   import Ecto.Changeset
 
+
   schema "manufacturers" do
     field :name, :string
     field :url, :string
@@ -13,6 +14,7 @@ defmodule FilmFlow.Settings.Manufacturer do
   def changeset(manufacturer, attrs) do
     manufacturer
     |> cast(attrs, [:name, :url])
-    |> validate_required([:name, :url])
+    |> validate_required([:name])
+    |> validate_format(:url, ~r/^https?\:\/\/[aA-zZ]+\.[aA-zZ]+\.[aA-zZ]{2,6}$/)
   end
 end
